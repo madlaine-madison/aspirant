099c9c0b7157d385f3a02ddf7fa45f15899de34d
Madlaine Madison (Maddy) <git@richter.wtf>
2025-02-05 21:17:45 +0100

    build: add bundles: orm, fixtures, maker, twig, ux-component, sass, test

--- config/bundles.php
+++ config/bundles.php
@@ -2,8 +2,24 @@
 
 declare(strict_types=1);
 
+use Doctrine\Bundle\DoctrineBundle\DoctrineBundle;
+use Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle;
+use Doctrine\Bundle\MigrationsBundle\DoctrineMigrationsBundle;
 use Symfony\Bundle\FrameworkBundle\FrameworkBundle;
+use Symfony\Bundle\MakerBundle\MakerBundle;
+use Symfony\Bundle\TwigBundle\TwigBundle;
+use Symfony\UX\TwigComponent\TwigComponentBundle;
+use Symfonycasts\SassBundle\SymfonycastsSassBundle;
+use Twig\Extra\TwigExtraBundle\TwigExtraBundle;
 
 return [
     FrameworkBundle::class => ['all' => true],
+    TwigBundle::class => ['all' => true],
+    TwigExtraBundle::class => ['all' => true],
+    TwigComponentBundle::class => ['all' => true],
+    SymfonycastsSassBundle::class => ['all' => true],
+    MakerBundle::class => ['dev' => true],
+    DoctrineBundle::class => ['all' => true],
+    DoctrineMigrationsBundle::class => ['all' => true],
+    DoctrineFixturesBundle::class => ['dev' => true, 'test' => true],
 ];
