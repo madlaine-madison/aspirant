4ccdc7f568b74977ee19c1a9156a64301c62492a
Madlaine Madison (Maddy) <git@richter.wtf>
2025-02-05 22:36:35 +0100

    feat(cli): add command to create (or overwrite) users

--- src/Command/UserCreateCommand.php
+++ src/Command/UserCreateCommand.php
@@ -5,7 +5,9 @@ declare(strict_types=1);
 namespace App\Command;
 
 use App\Entity\User;
+use App\Repository\UserRepository;
 use Doctrine\ORM\EntityManagerInterface;
+use Random\RandomException;
 use Symfony\Component\Console\Attribute\AsCommand;
 use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputInterface;
@@ -22,6 +24,7 @@ class UserCreateCommand extends Command
     public function __construct(
         private readonly EntityManagerInterface $entityManager,
         private readonly UserPasswordHasherInterface $passwordHasher,
+        private readonly UserRepository $userRepository,
     ) {
         parent::__construct();
     }
@@ -29,25 +32,46 @@ class UserCreateCommand extends Command
     protected function execute(InputInterface $input, OutputInterface $output): int
     {
         $io = new SymfonyStyle($input, $output);
-        $user = new User();
 
-        $user->setUsername(
-            $io->ask('Username')
-        );
+        $userName = $io->ask('Username');
+        $user = $this->userRepository->findOneBy(['username' => $userName]);
+        $existing = null !== $user;
+        if ($existing) {
+            $io->warning('Editing existing user!');
+        } else {
+            $user = new User();
+            $user->setUsername($userName);
+        }
+
+        $password = $io->askHidden('Password (leave blank for random password)');
+        $random = empty(trim($password ?? ''));
+        if ($random) {
+            try {
+                $password = bin2hex(random_bytes(32));
+            } catch (RandomException $e) {
+                $io->error($e->getMessage());
+            }
+        }
+
+        $admin = $io->confirm('Is admin?', \in_array('ROLE_ADMIN', $user->getRoles(), true));
+        if ($admin) {
+            $user->setRoles(['ROLE_ADMIN']);
+        }
 
         $user->setPassword(
             $this->passwordHasher->hashPassword(
                 $user,
-                $io->askHidden('Password')
+                $password
             )
         );
-
-        if ($io->confirm('Is admin?', false)) {
-            $user->setRoles(['ROLE_ADMIN']);
-        }
-
         $this->entityManager->persist($user);
         $this->entityManager->flush();
+
+        if ($random) {
+            if ($io->confirm('Show password?', $admin)) {
+                $io->writeln($password);
+            }
+        }
 
         return Command::SUCCESS;
     }
